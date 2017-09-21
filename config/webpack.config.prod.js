f8f7e551cab4b2c127540b542c849de5f3db932b
Marcin Modestowicz <marcin@modestowicz.com>
2017-09-21 16:20:29 +0200

    Add react-svg-loader

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -203,6 +203,10 @@ module.exports = {
             )
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.svg$/,
+            loader: require.resolve("svg-react-loader")
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader don't uses a "test" so it will catch all modules
